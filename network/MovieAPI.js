30c20ca26776c8fc5ee2b05881f9d91dee54b824
DivergentDev <dev@divergent.ng>
2019-10-26 00:36:29 +0100

    Added API Key from the movie db website

--- network/MovieAPI.js
+++ network/MovieAPI.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const IMG_URL_PREPEND = "https://image.tmdb.org/t/p/w500";
+const API_KEY = "31b3c9dba1a512f997a69f344b235e3d";
 
 class MovieAPI extends React.Component {
 
