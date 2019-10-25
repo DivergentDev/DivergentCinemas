e7b7e8edefb628e123a403e4387225e333aa5395
DivergentDev <dev@divergent.ng>
2019-10-26 00:35:38 +0100

    added image prepend URL

--- network/MovieAPI.js
+++ network/MovieAPI.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const IMG_URL_PREPEND = "https://image.tmdb.org/t/p/w500";
+
 class MovieAPI extends React.Component {
 
   constructor(props){
